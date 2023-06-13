ff4b84710bcb041f8051a7e82b9015323cb84ce5
josehermosilla <jose.hermosillaa@usach.cl>
2023-06-13 09:24:47 -0400

    se crea modelos Book y author, se registran en admin.py y se agrega la app primera a settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'primera',
     # aplicaciones de terceros
     'bootstrap5',
 ]
--- primera/models.py
+++ primera/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+TITLE_CHOICES = [
+    ('MR','Mr.'),
+    ('MRS','Mrs.'),
+    ('MS','Ms.'),
+]
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
